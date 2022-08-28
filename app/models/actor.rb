63e6a7f58e21d44eafa351260052936db6e25813
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-08-28 16:38:45 -0400

    commit tree 6d7e4f25ac85345fa0bba105765c1093e71df7a9

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
   
+    def full_name
+        name = "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        characters = Character.where("actor_id = ?", self.id)
+        roles = []
+        characters.each do |ch|
+            roles << "#{ch.name} - #{Show.find(ch.show_id).name}"
+        end
+        roles
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  def actors_list
+    characters = self.characters
+    actors = []
+    characters.each do |ch|
+        actors << Actor.find(ch.actor_id).full_name
+   end
+    actors
+
+end
+
 end
